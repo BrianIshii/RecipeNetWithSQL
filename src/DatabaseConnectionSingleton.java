8a88ad5e3ebe9d9392e42bd90afa34b4e402098e
danielcodin <daniel@danielcoding.com>
2019-03-03 23:07:40 -0800

    Change to the latest sql driver

--- src/DatabaseConnectionSingleton.java
+++ src/DatabaseConnectionSingleton.java
@@ -13,7 +13,7 @@ public class DatabaseConnectionSingleton {
             BufferedReader br = new BufferedReader(new FileReader(connectionsFile));
             connectionString = br.readLine();
 
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(connectionString);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
