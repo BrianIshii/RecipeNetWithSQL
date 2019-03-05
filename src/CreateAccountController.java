54ca0ce21309dea9e41428f35465608895fc683a
danielcodin <daniel@danielcoding.com>
2019-03-05 15:28:33 -0800

    Login and create account is functional

--- src/CreateAccountController.java
+++ src/CreateAccountController.java
@@ -1,4 +1,6 @@
 
+import Entity.User;
+import Service.UserService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +13,8 @@ import java.io.IOException;
 public class CreateAccountController {
 
     @FXML
+    private TextField nameTextField;
+    @FXML
     private TextField emailTextField;
     @FXML
     private TextField passwordTextField;
@@ -18,14 +22,26 @@ public class CreateAccountController {
     private TextField confirmPasswordTextField;
 
     @FXML
-    public void createAccountButtonPressed(ActionEvent event) {
-        emailTextField.setText("1");
-        passwordTextField.setText("2");
-        confirmPasswordTextField.setText("2");
+    public void createAccountButtonPressed(ActionEvent event) throws IOException {
+        // Check if passwords are the same
+        if (passwordTextField.getText().compareTo(confirmPasswordTextField.getText()) == 0) {
+            // Create user
+            UserService userService = UserService.getInstance();
+
+            userService.save(new User(nameTextField.getText(), emailTextField.getText(), passwordTextField.getText()));
+
+            // Transition to home view
+            Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+            Main.getPrimaryStage().getScene().setRoot(root);
+        } else {
+            // Alarm that the passwords do not match
+            System.out.println("Passwords don't match");
+        }
     }
 
     @FXML
     public void backButtonPressed(ActionEvent event) throws IOException {
+        // Transition to main view
         Parent root = FXMLLoader.load(getClass().getResource("MainController.fxml"));
         Main.getPrimaryStage().getScene().setRoot(root);
     }
--- src/MainController.java
+++ src/MainController.java
@@ -1,4 +1,6 @@
 
+import Entity.User;
+import Service.UserService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,18 +14,31 @@ public class MainController {
 
     @FXML
     private TextField emailTextField;
-
     @FXML
     private TextField passwordTextField;
 
+    private UserService userService = UserService.getInstance();
+
     @FXML
     public void loginButtonPressed(ActionEvent event) throws IOException {
-        emailTextField.setText("1");
-        passwordTextField.setText("2");
+        // Login
+        User authenticatedUser = userService.authenticate(emailTextField.getText(), passwordTextField.getText());
+
+        if (authenticatedUser != null) {
+            System.out.println(authenticatedUser);
+
+            // Transition to home view
+            Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+            Main.getPrimaryStage().getScene().setRoot(root);
+        } else {
+            // Alarm that the credentials are not valid
+            System.out.println("Credentials are not valid");
+        }
     }
 
     @FXML
     public void createAccountPressed(ActionEvent event) throws IOException {
+        // Transition to create account view
         Parent root = FXMLLoader.load(getClass().getResource("CreateAccountController.fxml"));
         Main.getPrimaryStage().getScene().setRoot(root);
     }
