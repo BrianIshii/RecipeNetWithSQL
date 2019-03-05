6ed3bdae5a1399e6bfb90d9cb840d63dee1146d3
danielcodin <daniel@danielcoding.com>
2019-03-05 13:03:05 -0800

    Add MainController

---
+++ src/MainController.java
@@ -0,0 +1,10 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    public void pressButton(ActionEvent event) {
+        System.out.println("Hello");
+    }
+}
